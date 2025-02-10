73737ef978462bb2d0f3cc335fa18ceaedc470fe
nicolocaruso85 <nicolaiphone@live.it>
2025-02-10 12:23:59 +0100

    Aggiornate dipendenze

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,9 @@ import cloud_firestore
 import connectivity_plus
 import device_info_plus
 import external_path_ios_mac
+import file_picker
 import file_selector_macos
+import firebase_analytics
 import firebase_auth
 import firebase_core
 import firebase_messaging
@@ -41,7 +43,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   ExternalPathIosMacPlugin.register(with: registry.registrar(forPlugin: "ExternalPathIosMacPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
