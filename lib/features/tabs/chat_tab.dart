8378660006845f715afe3eb4f198ad0af6be764f
nicolocaruso85 <nicolaiphone@live.it>
2025-03-07 23:15:50 +0100

    Aggiunto il pacchetto “auto_animated_list”

--- lib/features/tabs/chat_tab.dart
+++ lib/features/tabs/chat_tab.dart
@@ -7,6 +7,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
 import 'package:gap/gap.dart';
 import 'package:collection/collection.dart';
+import 'package:auto_animated_list/auto_animated_list.dart';
 
 import '../../../services/database.dart';
 import '../../helpers/extensions.dart';
@@ -38,6 +39,7 @@ class _ChatsTabState extends State<ChatsTab> {
         String idAzienda = snapshot.data!;
 
         _stream = FirebaseFirestore.instance.collection('users')
+          .where('isApproved', isEqualTo: true)
           .where('azienda', isEqualTo: FirebaseFirestore.instance.collection('aziende').doc(idAzienda))
           .snapshots();
 
@@ -64,109 +66,109 @@ class _ChatsTabState extends State<ChatsTab> {
 
             List sortedUsers = Map.fromEntries(tempUsers.entries.toList()..sort((e1, e2) => e1.key.compareTo(e2.key))).values.toList();
 
-            return ListView.builder(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: sortedUsers.length,
-              itemBuilder: (context, index) {
-                var doc = sortedUsers[index];
+            return AutoAnimatedList(
+              items: sortedUsers,
+              itemBuilder: (context, doc, index, animation) {
                 Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
 
                 if (_auth.currentUser!.email != data['email']) {
                   getUnreadMessagesCount(_auth.currentUser!.uid, data['uid']);
                   getLastMessageTime(_auth.currentUser!.uid, data['uid']);
 
-                  return ListTile(
-                    leading: data['profilePic'] != null && data['profilePic'] != ''
-                        ? Hero(
-                            tag: data['profilePic'],
-                            child: ClipOval(
-                              child: CachedNetworkImage(
-                                imageUrl: data['profilePic'],
-                                placeholder: (context, url) =>
-                                    Image.asset('assets/images/loading.gif'),
-                                errorWidget: (context, url, error) =>
-                                    const Icon(Icons.error_outline_rounded),
-                                width: 50.w,
-                                height: 50.h,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          )
-                        : Image.asset(
-                            'assets/images/user.png',
-                            height: 50.h,
-                            width: 50.w,
-                            fit: BoxFit.cover,
-                          ),
-                    tileColor: const Color(0xff111B21),
-                    title: Text(
-                      data['name'],
-                      style: const TextStyle(
-                        color: Colors.white,
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    subtitle: Text(
-                      data['isOnline']
-                          ? context.tr('online')
-                          : context.tr('offline'),
-                      style: const TextStyle(
-                        color: Color.fromARGB(255, 179, 178, 178),
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                    ),
-                    trailing: Column(
-                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        lastMessageTime[data['uid']] != null ?
-                          Text(
-                            formatDate(lastMessageTime[data['uid']]!),
-                            style: const TextStyle(
-                              color: Colors.white,
-                            ),
-                          ) : Column(),
-                        unreadMessagesCount[data['uid']] != null ?
-                          Column(
-                            children: [
-                              Gap(5.h),
-                              CircleAvatar(
-                                backgroundColor: Colors.red,
-                                maxRadius: 10,
-                                child: Center(
-                                  child: Text(
-                                    unreadMessagesCount[data['uid']].toString(),
-                                    style: TextStyles.font12White500Weight,
-                                  ),
+                  return SizeFadeTransition(
+                    animation: animation,
+                    child: ListTile(
+                      leading: data['profilePic'] != null && data['profilePic'] != ''
+                          ? Hero(
+                              tag: data['profilePic'],
+                              child: ClipOval(
+                                child: CachedNetworkImage(
+                                  imageUrl: data['profilePic'],
+                                  placeholder: (context, url) =>
+                                      Image.asset('assets/images/loading.gif'),
+                                  errorWidget: (context, url, error) =>
+                                      const Icon(Icons.error_outline_rounded),
+                                  width: 50.w,
+                                  height: 50.h,
+                                  fit: BoxFit.cover,
                                 ),
                               ),
-                            ],
-                          ) : Column(),
-                      ],
-                    ),
-                    isThreeLine: true,
-                    titleAlignment: ListTileTitleAlignment.center,
-                    enableFeedback: true,
-                    dense: false,
-                    titleTextStyle: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.sp,
-                      height: 1.2.h,
-                    ),
-                    subtitleTextStyle: TextStyle(
-                      height: 2.h,
-                    ),
-                    horizontalTitleGap: 15.w,
-                    onTap: () {
-                      context.pushNamed(Routes.chatScreen, arguments: data)
-                        .then((_) {
-                          setState(() {
-                            lastMessageTime = {};
-                            unreadMessagesCount = {};
+                            )
+                          : Image.asset(
+                              'assets/images/user.png',
+                              height: 50.h,
+                              width: 50.w,
+                              fit: BoxFit.cover,
+                            ),
+                      tileColor: const Color(0xff111B21),
+                      title: Text(
+                        data['name'],
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      subtitle: Text(
+                        data['isOnline']
+                            ? context.tr('online')
+                            : context.tr('offline'),
+                        style: const TextStyle(
+                          color: Color.fromARGB(255, 179, 178, 178),
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      ),
+                      trailing: Column(
+                         mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          lastMessageTime[data['uid']] != null ?
+                            Text(
+                              formatDate(lastMessageTime[data['uid']]!),
+                              style: const TextStyle(
+                                color: Colors.white,
+                              ),
+                            ) : Column(),
+                          unreadMessagesCount[data['uid']] != null ?
+                            Column(
+                              children: [
+                                Gap(5.h),
+                                CircleAvatar(
+                                  backgroundColor: Colors.red,
+                                  maxRadius: 10,
+                                  child: Center(
+                                    child: Text(
+                                      unreadMessagesCount[data['uid']].toString(),
+                                      style: TextStyles.font12White500Weight,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ) : Column(),
+                        ],
+                      ),
+                      isThreeLine: true,
+                      titleAlignment: ListTileTitleAlignment.center,
+                      enableFeedback: true,
+                      dense: false,
+                      titleTextStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.sp,
+                        height: 1.2.h,
+                      ),
+                      subtitleTextStyle: TextStyle(
+                        height: 2.h,
+                      ),
+                      horizontalTitleGap: 15.w,
+                      onTap: () {
+                        context.pushNamed(Routes.chatScreen, arguments: data)
+                          .then((_) {
+                            setState(() {
+                              lastMessageTime = {};
+                              unreadMessagesCount = {};
+                            });
                           });
-                        });
-                    },
+                      },
+                    ),
                   );
                 } else {
                   return const SizedBox.shrink();
