6da4f0ccf3bd005acbbc3a798bb186cd6232478c
nicolocaruso85 <nicolaiphone@live.it>
2025-03-08 00:31:17 +0100

    Aggiunta l’orario dell’ultimo messaggio anche per i gruppi

--- lib/features/tabs/groups_tab.dart
+++ lib/features/tabs/groups_tab.dart
@@ -4,6 +4,8 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:auto_animated_list/auto_animated_list.dart';
+import 'package:intl/intl.dart';
 
 import '../../helpers/extensions.dart';
 import '../../router/routes.dart';
@@ -16,72 +18,103 @@ class BuildGroupsListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: snapshot.data!.docs.length,
-      itemBuilder: (context, index) {
-        var doc = snapshot.data!.docs[index];
+    var sortedGroups = snapshot.data!.docs;
+
+    return AutoAnimatedList(
+      items: sortedGroups,
+      itemBuilder: (context, doc, index, animation) {
         Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
 
-        data['id'] = doc.id;
-
-        return ListTile(
-          leading: data['groupPic'] != null && data['groupPic'] != ''
-              ? Hero(
-                  tag: data['groupPic'],
-                  child: ClipOval(
-                    child: CachedNetworkImage(
-                      imageUrl: data['groupPic'],
-                      placeholder: (context, url) =>
-                          Image.asset('assets/images/loading.gif'),
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error_outline_rounded),
-                      width: 50.w,
-                      height: 50.h,
-                      fit: BoxFit.cover,
+        return SizeFadeTransition(
+          animation: animation,
+          child: ListTile(
+            leading: data['groupPic'] != null && data['groupPic'] != ''
+                ? Hero(
+                    tag: data['groupPic'],
+                    child: ClipOval(
+                      child: CachedNetworkImage(
+                        imageUrl: data['groupPic'],
+                        placeholder: (context, url) =>
+                            Image.asset('assets/images/loading.gif'),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error_outline_rounded),
+                        width: 50.w,
+                        height: 50.h,
+                        fit: BoxFit.cover,
+                      ),
                     ),
+                  )
+                : Image.asset(
+                    'assets/images/user.png',
+                    height: 50.h,
+                    width: 50.w,
+                    fit: BoxFit.cover,
                   ),
-                )
-              : Image.asset(
-                  'assets/images/user.png',
-                  height: 50.h,
-                  width: 50.w,
-                  fit: BoxFit.cover,
-                ),
-          tileColor: const Color(0xff111B21),
-          title: Text(
-            data['name'],
-            style: const TextStyle(
-              color: Colors.white,
+            tileColor: const Color(0xff111B21),
+            title: Text(
+              data['name'],
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              overflow: TextOverflow.ellipsis,
             ),
-            overflow: TextOverflow.ellipsis,
-          ),
-          subtitle: Text(
-            data['users'].length.toString() + ' ' + context.tr('users'),
-            style: const TextStyle(
-              color: Color.fromARGB(255, 179, 178, 178),
+            subtitle: Text(
+              data['users'].length.toString() + ' ' + context.tr('users'),
+              style: const TextStyle(
+                color: Color.fromARGB(255, 179, 178, 178),
+              ),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
             ),
-            overflow: TextOverflow.ellipsis,
-            maxLines: 1,
+            trailing: Text(
+              data['lastMessage'] != null ? formatDate(data['lastMessage']!.toDate()) : '',
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            isThreeLine: true,
+            titleAlignment: ListTileTitleAlignment.center,
+            enableFeedback: true,
+            dense: false,
+            titleTextStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.sp,
+              height: 1.2.h,
+            ),
+            subtitleTextStyle: TextStyle(
+              height: 2.h,
+            ),
+            horizontalTitleGap: 15.w,
+            onTap: () {
+              context.pushNamed(Routes.groupScreen, arguments: data);
+            },
           ),
-          isThreeLine: true,
-          titleAlignment: ListTileTitleAlignment.center,
-          enableFeedback: true,
-          dense: false,
-          titleTextStyle: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20.sp,
-            height: 1.2.h,
-          ),
-          subtitleTextStyle: TextStyle(
-            height: 2.h,
-          ),
-          horizontalTitleGap: 15.w,
-          onTap: () {
-            context.pushNamed(Routes.groupScreen, arguments: data);
-          },
         );
       },
     );
+  }
+
+  String formatDate(date) {
+    var year = DateFormat(DateFormat.YEAR, 'it_IT').format(date.toUtc());
+    var currentYear = DateFormat(DateFormat.YEAR, 'it_IT').format(DateTime.now());
+
+    var monthDay = DateFormat(DateFormat.MONTH_DAY, 'it_IT').format(date.toUtc());
+    var currentMonthDay = DateFormat(DateFormat.MONTH_DAY, 'it_IT').format(DateTime.now());
+
+    String formatedDate = '';
+    if (year == currentYear) {
+      if (monthDay == currentMonthDay) {
+        formatedDate = DateFormat(DateFormat.HOUR24_MINUTE, 'it_IT').format(date.toUtc());
+      }
+      else {
+        formatedDate = DateFormat(DateFormat.MONTH_DAY, 'it_IT').format(date.toUtc()) + ' ' + DateFormat(DateFormat.HOUR24_MINUTE, 'it_IT').format(date.toUtc());
+      }
+    }
+    else {
+      formatedDate = DateFormat(DateFormat.YEAR_MONTH_DAY, 'it_IT').format(date.toUtc()) + ' ' + DateFormat(DateFormat.HOUR24_MINUTE, 'it_IT').format(date.toUtc());
+    }
+
+    return formatedDate;
   }
 }
 
@@ -95,6 +128,7 @@ class GroupsTab extends StatelessWidget {
     return StreamBuilder<QuerySnapshot>(
       stream: FirebaseFirestore.instance.collection('groups')
         .where('users', arrayContains: FirebaseFirestore.instance.collection('users').doc(_auth.currentUser!.uid))
+        .orderBy('lastMessage', descending: true)
         .snapshots(),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
